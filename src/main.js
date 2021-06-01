13bbbeca4dad58895fbe8b7ab69ba9acb8eef0fb
chensongbai <1210828392@qq.com>
2021-06-01 20:09:02 +0800

    修改vscode 配置文件

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
 // import ChenButton from 'chen-button'
+Vue.use(ElementUI)
+
 // Vue.use(ChenButton)
 Vue.config.productionTip = false
 
