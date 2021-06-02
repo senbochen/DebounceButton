ae9edeb9d6debcab522508e18b9571b760da1b8e
chensongbai <1210828392@qq.com>
2021-06-02 09:52:19 +0800

    修改上传npm包的配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  indexPath: 'index.js',// 输出路径
   'lintOnSave': true,
   'chainWebpack': (config) => {
-    config.module.rule('eslint')
+    config.module.rule('eslint').rule('js')
+      .include
+      .add(__dirname + 'packages')
+      .end()
     config.module.rule('eslint').use('eslint-loader')
   }
 }
