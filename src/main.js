66090c33f6f1e71d941884070536c827b61c751e
chensongbai <1210828392@qq.com>
2021-06-02 10:02:43 +0800

    修改main.js文件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import ChenButton from 'chen-button'
 Vue.use(ElementUI)
-
-// Vue.use(ChenButton)
 Vue.config.productionTip = false
 
 new Vue({
